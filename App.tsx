1f35b9116aba1b26face0440db08359e8556bbde
bodie wood <bodie1@macbook-air.lan>
2023-05-11 12:25:47 -0700

    Add function to grab popular TV shows from TMDB API

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import {Text, View, StyleSheet} from 'react-native';
-import { Services } from './services/services';
+import {Text, View, StyleSheet, LogBox} from 'react-native';
+import { GetPopularMovies, GetUpcomingMovies, GetPopularTVShows } from './services/services';
+
 
 const styles = StyleSheet.create({
   center: {
@@ -13,20 +14,24 @@ const styles = StyleSheet.create({
 
 
 const App = () => {
+  LogBox.ignoreLogs(['Remote debugger']);
   const [movieTitle, setMovieTitle] = useState('');
   const [error, setError] = useState(false);
+  const [upcomingMovieTitle, setUpcomingMovieTitle] = useState('');
+
+
   //Promise will grab the first movie from the API and
   //set movieTitle to the title of that return.
   //useEffect makes sure that this process only occurs one time.
   useEffect(() => {
-    Services().then(movie => {
-    setMovieTitle(movie[0].original_title);
+    GetUpcomingMovies().then(movie => {
+    setUpcomingMovieTitle(movie[0].original_title);
   }).catch(err => {setError(err)});
 }, []);
   
   return (
     <View style={[styles.center, {top: 50}]}>
-      {error ? <Text style={[styles.errorStyle]}>Error in the system</Text> : <Text>{movieTitle}</Text>}
+      {error ? <Text style={[styles.errorStyle]}>Error in the system</Text> : <Text>{upcomingMovieTitle }</Text>}
     </View>
   );
 };
--- services/services.tsx
+++ services/services.tsx
@@ -1,10 +1,29 @@
 import axios from "axios";
 
-export const Services = async () =>
+//TODO: add apiKey to .env file
+const mainUrl:string = 'https://api.themoviedb.org/3'
+const apiKey:string = 'c19e6fff2c0692083ad7640cecadab90'
+
+//Grabs the popular movies from TMDB using their API.
+export const GetPopularMovies = async () =>
 {
-  const mainUrl:string = 'https://api.themoviedb.org/3'
-  const popularUrl:string = '/movie/popular';
-  const apiKey:string = 'c19e6fff2c0692083ad7640cecadab90'
-  const resp = await axios.get(`${mainUrl}${popularUrl}?api_key=${apiKey}`);
+  const popularMovies:string = '/movie/popular';
+  const resp = await axios.get(`${mainUrl}${popularMovies}?api_key=${apiKey}`);
   return resp.data.results;
 }
+//Grabs the upcoming movies from TMDB using their API.
+export const GetUpcomingMovies = async () => 
+{
+    const upcomingMovies: string = '/movie/upcoming'
+    const resp = await axios.get(`${mainUrl}${upcomingMovies}?api_key=${apiKey}`);
+    return resp.data.results;
+}
+
+//Grabs the popular TV shows from TMDB using their API.
+export const GetPopularTVShows = async () => 
+{
+    const popularTVShows: string = '/tv/popular'
+    const resp = await axios.get(`${mainUrl}${popularTVShows}?api_key=${apiKey}`);
+    return resp.data.results;
+}
+
